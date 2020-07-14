aeaaf95cd7fccdcdedf641223092b20bf0dbf64c
ioandinu <dinu.ioan@outlook.com>
2020-07-14 11:15:48 +0300

    Initial commit

---
+++ sync.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp -r ~/.config/{cef_user_data,gtk-2.0,gtk-3.0,libfm,neofetch,openbox,pcmanfm,termite,tint2} ./home/.config/
+cp -r ~/{.xmonad,.gtkrc-2.0,.nvidia-settings-rc,.xlayoutdisplay,.zshrc} ./home/
